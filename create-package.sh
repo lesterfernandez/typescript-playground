1cc89bdf5913a533b03ee06590bafc6a221db3dc
lesterfernandez <5958256+lesterfernandez@users.noreply.github.com>
2023-04-18 22:38:54 -0400

    Script to bootstrap project

---
+++ create-package.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+if [ ! $# -eq 1 ] || [[ $1 = *" "* ]]
+then
+  echo "Usage: create-package <package>"
+  exit 1
+fi
+
+mkdir $1
+cp base/tsconfig.json $1
+cat base/package.json | sed "s/base/$1/" > $1/package.json
