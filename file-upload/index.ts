bd0444b08a53c318a22a5c808c755c4368804cc2
lesterfernandez <5958256+lesterfernandez@users.noreply.github.com>
2023-04-19 22:42:53 -0400

    finished file-upload test

--- file-upload/index.ts
+++ file-upload/index.ts
@@ -1,9 +1,22 @@
 import fs from "node:fs";
 import http from "node:http";
 
+// I think it is really cool how you can pipe streams in node.js
+// This feature is not used much but it's super convenient, it feels very UNIX-like
+
 const server = http.createServer((req, res) => {
-  if (req.url == "/") {
+  if (req.url === "/" && req.method === "GET") {
     fs.createReadStream("index.html").pipe(res);
+    return;
+  }
+
+  if (req.method === "POST") {
+    req.pipe(fs.createWriteStream("test.jpg"));
+    req.on("end", () => {
+      console.log("we are done here bois");
+      res.statusCode = 201;
+      res.end();
+    });
   }
 });
 
