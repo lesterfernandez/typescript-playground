536c77c5b8de57040c8054e543ce9902a06021e6
lesterfernandez <5958256+lesterfernandez@users.noreply.github.com>
2023-04-22 00:36:22 -0400

    basic video streaming

--- file-upload/index.ts
+++ file-upload/index.ts
@@ -4,19 +4,24 @@ import http from "node:http";
 // I think it is really cool how you can pipe streams in node.js
 // This feature is not used much but it's super convenient, it feels very UNIX-like
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   if (req.url === "/" && req.method === "GET") {
     fs.createReadStream("index.html").pipe(res);
     return;
   }
 
-  if (req.method === "POST") {
+  if (req.url === "/upload" && req.method === "POST") {
     req.pipe(fs.createWriteStream("test.jpg"));
     req.on("end", () => {
       console.log("we are done here bois");
       res.statusCode = 201;
       res.end();
     });
+  }
+
+  if (req.url === "/video") {
+    res.setHeader("Content-Type", "video/mp4");
+    fs.createReadStream("fish-vid.mp4").pipe(res);
   }
 });
 
